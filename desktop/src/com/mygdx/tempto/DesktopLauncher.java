d9218024110059e2f5b145a25107a4d5536e2364
Temp <temporal.zip@gmail.com>
2024-05-21 22:38:45 -0500

    Initiate Project from LibGDX installer

---
+++ desktop/src/com/mygdx/tempto/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.tempto;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.tempto.TemptoNova;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Tempto Nova");
+		new Lwjgl3Application(new TemptoNova(), config);
+	}
+}
