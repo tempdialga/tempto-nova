aa6e91d1d6506d4c79e422bdeb7bb3d66ccf3910
Temp Dialga <temporal.zip@gmail.com>
2025-05-22 14:12:17 +0800

    Added assets, which we were procrastinating on due to theoretical size constraints.

---
+++ data/maps/padded-tiles.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="padded-tiles" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="400" columns="20">
+ <image source="padded-tiles.png" width="360" height="360"/>
+ <tile id="41">
+  <animation>
+   <frame tileid="41" duration="350"/>
+   <frame tileid="42" duration="350"/>
+   <frame tileid="43" duration="350"/>
+   <frame tileid="44" duration="350"/>
+   <frame tileid="45" duration="350"/>
+   <frame tileid="46" duration="350"/>
+   <frame tileid="47" duration="350"/>
+   <frame tileid="48" duration="350"/>
+   <frame tileid="49" duration="350"/>
+  </animation>
+ </tile>
+ <tile id="51">
+  <animation>
+   <frame tileid="51" duration="350"/>
+   <frame tileid="52" duration="350"/>
+   <frame tileid="53" duration="350"/>
+   <frame tileid="54" duration="350"/>
+   <frame tileid="55" duration="350"/>
+   <frame tileid="56" duration="350"/>
+  </animation>
+ </tile>
+ <tile id="272">
+  <animation>
+   <frame tileid="273" duration="750"/>
+   <frame tileid="274" duration="1500"/>
+  </animation>
+ </tile>
+</tileset>
